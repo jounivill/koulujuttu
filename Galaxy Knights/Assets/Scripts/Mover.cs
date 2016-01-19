3191c674c80dbc91bf2cad99f9a787411b094b7c
Jouni Villberg <jouni.villberg@gmail.com>
2016-01-19 08:44:13 +0200

    Unity projekti

---
+++ Galaxy Knights/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody2D rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = transform.up * speed;
+	}
+}
+
---
+++ Galaxy Knights/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public float tilt;
+
+	public GameObject Shot;
+	public Transform ShotSpawn;
+	public float fireRate;
+
+	private Rigidbody2D rb;
+	private float nextFire;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKey("space") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (Shot, ShotSpawn.position, ShotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		rb.velocity = movement * speed;
+
+		if (transform.position.x <= -3.8f) {
+			transform.position = new Vector2(-3.8f, transform.position.y);
+		} else if (transform.position.x >= 3.8f) {
+			transform.position = new Vector2(3.8f, transform.position.y);
+		}
+			
+		if (transform.position.y <= -1.5f) {
+			transform.position = new Vector2(transform.position.x, -1.5f);
+		} else if (transform.position.y >= 6.0f) {
+			transform.position = new Vector2(transform.position.x, 6.0f);
+		}
+}
+}
